16345771f7328f710347142fc7bf63fd55c57be2
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-07-09 19:53:40 +0300

    fix

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,33 +1,42 @@
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
-
-
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export function createGallery(imagesData) {
-  const gallery = document.querySelector(".gallery");
+  const gallery = document.querySelector('.gallery');
 
   // Очистка текущего содержимого галереи
-  gallery.innerHTML = "";
+  gallery.innerHTML = '';
 
   // Создание HTML строк для всех изображений
-  const galleryHTML = imagesData.map(image => `
-    <a href="${image.largeImageURL}" title="${image.tags}">
-      <img src="${image.webformatURL}" alt="${image.tags}">
-    </a>
-  `).join('');
+  const galleryHTML = imagesData
+    .map(
+      image => `
+    <div class="card">
+      <a href="${image.largeImageURL}" title="${image.tags}">
+        <img src="${image.webformatURL}" alt="${image.tags}">
+      </a>
+      <div class="card-info">
+        <p>Likes: ${image.likes}</p>
+        <p>Views: ${image.views}</p>
+        <p>Comments: ${image.comments}</p>
+        <p>Downloads: ${image.downloads}</p>
+      </div>
+    </div>
+  `
+    )
+    .join('');
 
   // Вставка HTML строки в контейнер галереи
   gallery.insertAdjacentHTML('beforeend', galleryHTML);
 
   // Инициализация SimpleLightbox
-  const lightbox = new SimpleLightbox(".gallery a", {
-    captionsData: "title",
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
     captionDelay: 250,
     close: true,
-    nav: true
+    nav: true,
+
     // Другие параметры настройки по вашему выбору
   });
-   
 }
